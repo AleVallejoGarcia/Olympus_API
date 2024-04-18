f13bfa6cc47b917dd93906749657a153561ce40a
AleVallejoGarcia <alevalga@gmail.com>
2024-04-18 17:23:17 +0100

    feat:(created a method to find by name a user)

--- src/main/java/com/olympus/repositories/UserRepository.java
+++ src/main/java/com/olympus/repositories/UserRepository.java
@@ -9,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User findByName(String name);
+
 }
