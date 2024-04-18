336e123c04fce4532e49b7b073e8964494fba2a2
AleVallejoGarcia <alevalga@gmail.com>
2024-04-18 17:02:47 +0100

    feat & fix:(created both repositoreis the admin and user ones, fixed an issue with the pom

---
+++ src/main/java/com/olympus/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.olympus.repositories;
+
+import org.springframework.stereotype.Repository;
+
+import com.olympus.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
