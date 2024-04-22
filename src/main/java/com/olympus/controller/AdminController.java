29b2da0df39ca37f8544865e2778c82832018d76
AleVallejoGarcia <alevalga@gmail.com>
2024-04-22 18:52:57 +0100

    feat(added new endpoint to admin and routines controller)

--- src/main/java/com/olympus/controller/AdminController.java
+++ src/main/java/com/olympus/controller/AdminController.java
@@ -1,5 +1,41 @@
 package com.olympus.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.olympus.models.Admin;
+import com.olympus.repositories.AdminRepository;
+
+import com.olympus.exception.ResourceNotFoundException;
+
+@RestController
+@RequestMapping("/olympus/v1")
 public class AdminController {
 
+    @Autowired
+    AdminRepository adminRepository;
+
+    public void deleteAdmin(@PathVariable Long id) {
+        try {
+            adminRepository.deleteById(id);
+        } catch (Exception e) {
+            System.out.println("Admin Id " + id + " not found");
+        }
+    }
+
+    @PutMapping("/user/{id}")
+    public Admin updateAdminById(@PathVariable Long id, @RequestBody Admin adminRequest) {
+        return adminRepository.findById(id).map(admin -> {
+            admin.setName(adminRequest.getName());
+            admin.setMail(adminRequest.getMail());
+            admin.setPassword(adminRequest.getPassword());
+
+            return adminRepository.save(admin);
+        }).orElseThrow(
+                () -> new ResourceNotFoundException("User not found with id " + id));
+    }
 }
--- src/main/java/com/olympus/controller/RoutineController.java
+++ src/main/java/com/olympus/controller/RoutineController.java
@@ -1,6 +1,7 @@
 package com.olympus.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +20,14 @@ public class RoutineController {
     @GetMapping("/routines/{userId}")
     public Routine getRoutineByUsedId(@PathVariable Long userId) {
         return routineRepository.findByUserId(userId);
+    }
+
+    @DeleteMapping("/routines/{id}")
+    public void deleteRoutine(@PathVariable Long id) {
+        try {
+            routineRepository.deleteById(id);
+        } catch (Exception e) {
+            System.out.println("Routine Id " + id + " not found");
+        }
     }
 }
