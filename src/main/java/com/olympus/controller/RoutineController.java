e6c25e65cd519514032ee332dfbb8a93cbd731b8
AleVallejoGarcia <alevalga@gmail.com>
2024-04-23 07:53:45 +0100

    added a post ending to RoutineController

--- src/main/java/com/olympus/controller/RoutineController.java
+++ src/main/java/com/olympus/controller/RoutineController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,4 +34,10 @@ public class RoutineController {
             System.out.println("Routine Id " + id + " not found");
         }
     }
+
+    @PostMapping("/routines/{userId}")
+    public Routine addRoutineToUser(@RequestBody Routine routineRequest) {
+        return routineRepository.save(routineRequest);
+    }
+
 }
