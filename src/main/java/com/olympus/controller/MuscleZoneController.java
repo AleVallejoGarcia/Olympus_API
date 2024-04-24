95f48ab6133ef8f4966613bce7a5164d30acbbcf
rica07b <rica07b@gmail.com>
2024-04-24 18:17:19 +0100

    feat: added get endpoints for muscle zone

---
+++ src/main/java/com/olympus/controller/MuscleZoneController.java
@@ -0,0 +1,34 @@
+package com.olympus.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.olympus.models.Achievement;
+import com.olympus.models.MuscleZone;
+import com.olympus.repositories.AchievementsRepository;
+import com.olympus.repositories.MuscleZoneRepository;
+
+@RestController
+@RequestMapping("/olympus/v1")
+public class MuscleZoneController {
+
+	@Autowired
+    MuscleZoneRepository muscleZoneRepository;
+    
+    @GetMapping("/muscle_zone")
+    public List<MuscleZone> getAllMuscleZones() {
+        return muscleZoneRepository.findAll();
+    }
+    
+    @GetMapping("/muscle_zone/{id}")
+    public Optional<MuscleZone> getMuscleZonesById(@PathVariable Long id) {
+        return muscleZoneRepository.findById(id);
+    }
+	
+}
---
+++ src/main/java/com/olympus/repositories/MuscleZoneRepository.java
@@ -0,0 +1,10 @@
+package com.olympus.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.olympus.models.MuscleZone;
+
+public interface MuscleZoneRepository extends JpaRepository<MuscleZone, Long>{
+	
+	
+}
